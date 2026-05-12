24ce8ed577054aafa8ae0215b86791c18f816346
Fouda-Emmanuel <leoemmanuelson46@gmail.com>
2026-05-12 19:05:47 +0100

    chore: add frontend envs, configure frontend/backend Dockerfiles & add Docker Compose setup

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django_prometheus.db.backends.postgresql",
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
     }
 }
 
---
+++ docker/dev/django/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+set -o nounset
+
+python manage.py migrate --noinput
+
+python manage.py collectstatic --noinput
+
+exec python manage.py runserver 0.0.0.0:8000
